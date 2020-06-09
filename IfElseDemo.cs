f9ba431db829062137305050e5b12116181016e7
Nitesh Shaw <37259465+nit-esh@users.noreply.github.com>
2020-06-09 16:51:18 +0530

    Add files via upload

---
+++ IfElseDemo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatementDemoProject
+{
+    class IfElseDemo
+    {
+        static void Main(string[] args)
+        {
+            // We use if-else to excecute blocks of code which satisfy certain conditions.
+
+            double marks=0;
+
+            #region While Loop Example
+            while(true)
+            {
+                Console.WriteLine("Enter the marks : ");
+                string value = Console.ReadLine();
+
+                marks = double.Parse(value);
+
+                if (marks <= 35)
+                {
+                    Console.WriteLine("Better Luck next time !");
+                    Console.WriteLine("Work hard....");
+                }
+
+                else
+                {
+                    Console.WriteLine("Congratulations !");
+                    Console.WriteLine("You are promoted.\n");
+                }
+
+                Console.WriteLine("Want to enter more marks ? y/n ");
+                string check = Console.ReadLine();
+                if (check == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            #endregion
+
+            #region For-loop example
+            //for (; ; )
+            //{
+            //    Console.WriteLine("Enter the marks : ");
+            //    string value = Console.ReadLine();
+
+            //    marks = double.Parse(value);
+
+            //    if (marks <= 35)
+            //    {
+            //        Console.WriteLine("Better Luck next time !");
+            //        Console.WriteLine("Work hard....");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("Congratulations !");
+            //        Console.WriteLine("You are promoted.\n");
+            //    }
+
+            //    Console.WriteLine("Want to enter more marks ? y/n ");
+            //    string check = Console.ReadLine();
+            //    if (check == "y")
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Are you sure? y/n");
+            //        string checkAgain=Console.ReadLine();
+            //        if (checkAgain=="n")
+            //        {
+            //            continue;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+                
+               
+            //}
+
+            #endregion
+
+        }
+    }
+}
---
+++ SwitchCaseDemo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatementDemoProject
+{
+    public class SwitchCaseDemo
+    {
+        static void Main(string[] args)
+        {
+            #region Switch-Case Demo
+            //while (true)
+            //{
+            //    int dayOfWeek;
+            //    Console.WriteLine("Enter the day number: ");
+            //    dayOfWeek = int.Parse(Console.ReadLine());
+
+            //    switch (dayOfWeek)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("It is Sunday !");
+            //            goto case 1; 
+
+            //        case 2:
+            //            Console.WriteLine("It is Monday !");
+            //            goto case 3;
+                        
+            //        case 3:
+            //            Console.WriteLine("It is Tuesday !");
+            //            goto case 4;
+                       
+            //        case 4:
+            //            Console.WriteLine("It is Wednesday !");
+            //            goto case 5;
+                        
+            //        case 5:
+            //            Console.WriteLine("It is Thursday !");
+            //            break;
+                        
+            //        case 6:
+            //            Console.WriteLine("It is Friday !");
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("It is Saturday !");
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Invalid day number. Kindly enter the number between 1-7");
+            //            break;
+
+            //    }
+
+                
+            //    Console.WriteLine("Do you want to enter another number?y/n");
+
+            //    string check = Console.ReadLine();
+            //    if (check=="y")
+            //    {
+            //        continue;
+                    
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+            #endregion
+
+            var num = 5;
+            while(num<=50)
+            {
+                Console.WriteLine(num) ;
+                num += 5;
+
+            }
+        }
+    }
+}
