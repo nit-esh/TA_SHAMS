d7c15d383cdbf743fcf4b23e4eac3b7276931c8b
Nitesh Shaw <37259465+nit-esh@users.noreply.github.com>
2020-06-12 16:56:20 +0530

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book()
+        {
+            grades = new List<double>();
+            //Console.WriteLine("An Object is created");
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+       public void DisplayGrade()
+        {
+            Console.WriteLine("The grades are :");
+            foreach (var grade in grades)
+            {
+                
+                Console.WriteLine(grade);
+            }
+        }
+
+        public void ShowStatistics()
+        {
+            var highestGrade = double.MinValue;
+
+            foreach (var grade in grades)
+            {
+                highestGrade = Math.Max(highestGrade, grade);
+
+            }
+            Console.WriteLine("The highest Grade is :{0}",highestGrade);
+
+            var lowestGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                lowestGrade = Math.Min(lowestGrade, grade);
+
+            }
+            Console.WriteLine("The lowest grade is :{0}",lowestGrade);
+
+            double sum = 0;
+            var gradeCount = grades.Count;
+            foreach (var grade in grades)
+            {
+                sum = sum + grade;
+            }
+            var average = sum / gradeCount;
+
+            Console.WriteLine("The average grade is :{0}", average);
+        }
+
+        List<double> grades;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book();
+
+            //Add the grades in the book
+            book.AddGrade(58.3);
+            book.AddGrade(60.8);
+            book.AddGrade(98.2);
+
+            //Display the grades of a book
+            book.DisplayGrade();
+
+            //Shows the Highest, Lowest and Average grades of a class
+            book.ShowStatistics();
+
+        }
+
+
+
+           
+        
+    }
+}
