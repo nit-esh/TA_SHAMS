f9e9a5cc8df81d2842772a08e63713232c6fc0a4
Nitesh Shaw <37259465+nit-esh@users.noreply.github.com>
2020-06-17 17:55:40 +0530

    Add files via upload

---
+++ TA Project Demo/CountryDemoProject/CountryDemoProject/Country.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryDemoProject
+{
+    class Country
+    {
+        public string Name { get; }
+        public string Code { get; }
+        public string Continent { get;}
+        public int Population { get; }
+
+        //Propetirs
+        //Methods
+        //Fields
+        //Events
+        public Country(string name, string code, string continent, int population)
+        {
+            Name = name;
+            Code = code;
+            Continent = continent;
+            Population = population;
+        }
+       
+
+    }
+}
---
+++ TA Project Demo/CountryDemoProject/CountryDemoProject/CsvReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryDemoProject
+{
+    class CsvReader
+    {
+        public string FilePath { get; }
+
+        public CsvReader(string csvFilePath)
+        {
+            FilePath = csvFilePath;
+        }
+
+        public Country[] ReadNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                
+                sr.ReadLine(); //read the header line
+                
+                for (int i = 0; i < nCountries; i++)
+                {
+                    var csvline = sr.ReadLine();
+                    countries[i] = CsvReader.ReadSeperateData(csvline);
+                    
+                }
+                return countries;
+            };
+           
+            
+            
+        }
+
+        private static Country ReadSeperateData(string csvline)
+        {
+           
+            string[] parts = csvline.Split(new char[] { ',' });
+            
+            return new Country(parts[0], parts[1], parts[2], int.Parse(parts[3]));
+
+        }
+    }
+}
