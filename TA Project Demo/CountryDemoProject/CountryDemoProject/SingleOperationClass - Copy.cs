a5f27298bc8502bffaf238a0b19391358f0c626e
Nitesh Shaw <37259465+nit-esh@users.noreply.github.com>
2020-06-18 16:50:14 +0530

    Add files via upload

---
+++ TA Project Demo/CountryDemoProject/CountryDemoProject/SingleOperationClass - Copy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CountryDemoProject
+{
+    class SingleOperationClassNew
+    {
+
+        static void Main(string[] args)
+        {
+
+            string filePath = "G:\\countries.txt";
+
+            StreamReader sr = new StreamReader(filePath);
+            //sr.ReadLine();
+            Console.WriteLine("Enter the number of countries to be displayed: >");
+            var nCountries = int.Parse(Console.ReadLine());  
+            for (int i = 1; i <= nCountries; i++)
+            {
+                var csvline=sr.ReadLine();
+                string[] parts= csvline.Split(',');
+
+                string name = parts[0];
+                string code = parts[1];
+                string continent = parts[2];
+                int population = int.Parse(parts[3]);
+                //int density = int.Parse(parts[4]);
+
+                Console.WriteLine($"Country: {name}, Code: {code}, Continent: {continent}, Population : {population}");
+            }
+;        }
+    }
+}
