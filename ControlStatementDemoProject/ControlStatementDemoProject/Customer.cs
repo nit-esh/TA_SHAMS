1ced09609a6c6664bc0f0a445dc913ad866c38d9
Nitesh Shaw <37259465+nit-esh@users.noreply.github.com>
2020-07-17 15:43:01 +0530

    Add files via upload

---
+++ ControlStatementDemoProject/ControlStatementDemoProject/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatementDemoProject
+{
+    public class Customer
+    {
+        //fields
+        public static string firstname { get; set; }
+        //properties
+
+        //methods
+
+        //events
+    }
+    
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Customer.firstname = "Nitesh";
+            Customer customer = new Customer();
+           
+
+            Customer customer_new = new Customer();
+            customer_new = customer;
+
+           
+
+            Console.WriteLine(Customer.firstname);
+
+            int i = 5;
+            int j = i;
+            j = 10;
+            Console.WriteLine(i); ;
+
+
+            
+
+        }
+    }
+    
+}
